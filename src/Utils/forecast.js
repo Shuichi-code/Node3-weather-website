71c1afdc67702ccdd9067641718cebce651d26f7
Shuichi-code <erick.vanjoeff.misoles@gmail.com>
2021-10-17 10:50:28 +0800

    Added the humidity to the forecast data and replaced darksky to weatherstack in the about page.

--- src/Utils/forecast.js
+++ src/Utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+
+
 const forecast = (longitude, latitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=46415e4fa6461ccbc3e7e51c5b44fe79&query='+ latitude +','+ longitude +'&units=f';
 
@@ -10,7 +12,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location', undefined);
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.");
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is "+body.current.humidity+".");
         }
     });
 }
